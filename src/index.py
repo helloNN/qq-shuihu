e6d60b4c1785f0fd649a80558ed1b6befd22382d
nn-desktop <2468659059@qq.com>
2025-08-23 14:17:36 +0800

    feat: 方案研究

--- src/index.py
+++ src/index.py
@@ -2,6 +2,12 @@ from utils.hwnd import GameHwnd
 from pywinauto import Application
 
 
+def getPosition(rect):
+    return (rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top)
+
+
+# 后续思路， 通过 handle 得到多个 app ，然后处理
+# handle 手动获取, 目前无法通过代码一次夺取全都符合条件的窗口句柄
 def main():
     game1 = GameHwnd()
     hwnd = game1.find_hwndByTitle_p_child("MainWindow", "Chrome Legacy Window")
@@ -12,44 +18,21 @@ def main():
 
     app = Application(backend="uia").connect(handle=hwnd)
     mainWindow = app.MainWindow
-
-    qq254 = mainWindow.child_window(
-        title="2548918215", auto_id="btnTab", control_type="Button"
-    )
-    qq24686 = mainWindow.child_window(
-        title="醒来，遇见你", auto_id="btnTab", control_type="Button"
-    )
-    qq3305 = mainWindow.child_window(
-        title="nn", auto_id="btnTab", control_type="Button"
-    )
-    # qq254.print_control_identifiers()
-    # qq24686.print_control_identifiers()
-    # qq3305.print_control_identifiers()
-
-    sixBar = mainWindow.child_window(auto_id="nav1", control_type="List")
-    # sixBar.print_control_identifiers()
-    guanfangWeb = sixBar.child_window(title="官方网站", control_type="Hyperlink")
-    rect = guanfangWeb.rectangle()
-    # print(guanfangWeb.rectangle())
-
-    origin = (rect.left, rect.bottom)
-    print(f"游戏原点: {origin}")
-
-    flash_content = app.window(auto_id="flashContent", control_type="Group")
-    if flash_content.exists():
-        print("找到了 flashContent")
-    else:
-        print("没有找到 flashContent")
-
-    # gameWindow.drwaw_outline(colour="red", thickness=2)
-    # xx = mainWindow.Pane2
-    # xx.print_control_identifiers()
-
     # mainWindow.print_control_identifiers()
-    # print(app.windows())
-    # game1.set_hwnd(hwnd)
 
-    # game1.click(985, 155, "left")
+    # 联系官方，下面那块区域
+    contactArea = mainWindow.child_window(
+        auto_id="windowsFormsHost", control_type="Pane"
+    )
+    contactAreaRect = contactArea.rectangle()
+    print(f"联系官方：下面整块区域: {getPosition(contactAreaRect)}")
+    # contactArea.click_input(coords=(528, 366))     可以正常点击
+
+    # 游戏内falsh区域
+    flashArea = mainWindow["Custom4"]
+    flashAreaRect = flashArea.rectangle()
+    print(f"游戏坐标信息: {getPosition(flashAreaRect)}")
+    # flashArea.click_input(coords=(546, 328))         也可以点击
 
 
 if __name__ == "__main__":
--- test/index.py
+++ test/index.py
@@ -1,6 +1,7 @@
 import json
 from pywinauto import Application
 import sys
+import psutil
 
 
 # 可选：辅助函数计算元素总数
@@ -80,9 +81,26 @@ def dump_to_json(element, filename, depth=10):
 # 使用示例
 if __name__ == "__main__":
     # 连接到应用程序
-    app = Application(backend="uia").connect(handle=524372)
-    main_window = app.MainWindow
+    app = Application(backend="win32").connect(process=20732)
+    mainWindow = app.MainWindow
+    mainWindow.print_control_identifiers()
 
-    # 输出为JSON
-    json_data = dump_to_json(main_window, "control_tree.json", depth=8)
-    print(f"控件树已保存为JSON，包含 {count_elements(json_data)} 个元素")
+    # 使用进程名获取所有相关进程
+    process_name = "qq108miniwpfn45.exe"
+    target_processes = []
+
+    for proc in psutil.process_iter(["pid", "name"]):
+        if proc.info["name"].lower() == process_name.lower():
+            target_processes.append(
+                {"pid": proc.info["pid"], "name": proc.info["name"]}
+            )
+
+    for p in target_processes:
+        print(f"PID: {p['pid']}, 进程名: {p['name']}")
+
+    # xxx = mainWindow["Chrome_WidgetWin_0"]
+    # xxx.print_control_identifiers()
+
+    # # 输出为JSON
+    # json_data = dump_to_json(main_window, "control_tree.json", depth=8)
+    # print(f"控件树已保存为JSON，包含 {count_elements(json_data)} 个元素")
